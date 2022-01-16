cbcac7e6b06867f2710cf574175123a19e3e28e5
olich97 <olich.me@gmail.com>
2022-01-16 15:04:33 +0100

    feat: land contract tests

---
+++ vland/erc-721/test/testLand.ts
@@ -0,0 +1,44 @@
+
+import {ethers, waffle} from 'hardhat';
+import chai from 'chai';
+
+import landArtifact from '../artifacts/contracts/Land.sol/Land.json';
+import {Land} from '../typechain-types/Land';
+import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
+
+const {deployContract} = waffle;
+const {expect} = chai;
+
+describe('Land', function () {
+  let contractOwner: SignerWithAddress;
+  let nftOwner1: SignerWithAddress;
+  let nftOwner2: SignerWithAddress;
+  let addrs: SignerWithAddress[];
+
+  let land: Land;
+
+  beforeEach(async () => {
+    // eslint-disable-next-line no-unused-vars
+    [contractOwner, nftOwner1, nftOwner2, ...addrs] = await ethers.getSigners();
+
+    land = (await deployContract(contractOwner, landArtifact)) as unknown as Land;
+  });
+ 
+  it("Should set the right owner", async () => {
+    expect(await land.owner()).to.equal(contractOwner.address);
+  });
+
+  it("Should correctly mint land nft", async function () {
+    const landContract = await land.connect(contractOwner);
+    const tokenMetadataUrl = 'http://www.example/tokens/1/metadata.json';
+    
+    expect(
+      await landContract.mintLand(nftOwner1.address, tokenMetadataUrl)
+    )
+    .to.emit(land, "Transfer")
+    .withArgs(ethers.constants.AddressZero, nftOwner1.address, 1);
+      
+    const tokenUri = await landContract.tokenURI(1);
+    expect(tokenUri).to.equal(tokenMetadataUrl);
+  });
+});
