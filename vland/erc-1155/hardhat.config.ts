d642f145efec4b8d84c2fb623be8ef82f81ec8e7
olich97 <olich.me@gmail.com>
2022-01-27 20:24:32 +0100

    feat(erc-1155): add base structure

---
+++ vland/erc-1155/hardhat.config.ts
@@ -0,0 +1,61 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      // property of the solidity compiler
+      // maximize gas cost savings by taking into consideration how many times the bytecode is expected to run (lifetime) 
+      // and the cost of introducing an optimization at compile time
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    coinmarketcap: process.env.REPORT_GAS_COIN_MARKET_CAP_KEY || ""
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  } 
+};
+
+export default config;
